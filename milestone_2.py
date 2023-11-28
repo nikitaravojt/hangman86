27f47d2cb5c4c83234af58a4e0d5a941c6dfe858
Nikita Ravojt <nikitaravojt@outlook.com>
2023-11-28 16:43:03 +0000

    Added guess() func

--- milestone_2.py
+++ milestone_2.py
@@ -1,3 +1,16 @@
+import random
+
+
+def guess():
+    while True:
+        user_guess = input("Your guess: ").lower()
+        if user_guess.isalpha() and len(user_guess) == 1:
+            return user_guess
+        else:
+            print("Please enter a single letter!")
+
 
 word_list = ["apple", "banana", "kiwi", "passionfruit", "lychee"]
-print(word_list)
+word = random.choice(word_list)
+guess1 = guess()
+print(guess1)
